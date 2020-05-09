67df02c5dc6335663eeb7dad0103c3a3441c53f3
Stanko Bebek <stanko.bebek82@gmail.com>
2020-05-10 01:48:36 +0200

    optimized stuff on Home.vue and made it responsive to all devices

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -11,57 +11,9 @@
 
 <style scoped>
 
-.bottom{
-position: fixed;
-bottom: 0;
-background-color:#ffcc00;
-width: 100%;
-box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
-}
-p{
-    color:white;
+.bv-example-row{
     text-align: center;
-    font-family:'Courier New', Courier, monospace ;
-    margin-top: 5px;
-    margin-bottom: 5px;
 }
-pre{
-    color:white;
-    text-align: center;
-    font-size: 25px;
-    font-family:'Courier New', Courier, monospace ;
-    margin-top: 5px;
-    margin-bottom: 5px;
-
-}
-.space{
-    font-family:Courier;
-    white-space:pre;
-}
-@media screen and (max-width: 1000px) {
-    pre{
-    color:white;
-    text-align: center;
-    font-size: 20px;
-    font-family:'Courier New', Courier, monospace ;
-    margin-top: 5px;
-    margin-bottom: 5px;
-    }
-
-}
-@media screen and (max-width: 780px) {
-    pre{
-    color:white;
-    text-align: center;
-    font-size: 13px;
-    font-family:'Courier New', Courier, monospace ;
-    margin-top: 5px;
-    margin-bottom: 5px;
-    }
-
-}
-
-
 
 
 </style> 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -18,13 +18,14 @@
             </div>
         </div>
         <img src="../assets/dashboard/l-side.png" class="side_img"/>
-        
-        <br><br><br>
 
         <div class="boxCard">
-            <b-container class="bv-example-row">
+          <div class="mb-5 text-center">
+              <h3 class="card-info">Learn more about our app</h3>
+          </div>
+            <div class="bv-example-row">
               <b-row>
-                <b-col class="col">
+                <b-col sm="12" md="4">
                   <div class="box">
                     <b-card
                       title="Singleplayer"
@@ -32,7 +33,7 @@
                       img-alt="Image"
                       img-top
                       tag="article"
-                      style="max-width: 20rem;"
+                      style="max-width: 50rem;"
                       class="mb-2"
                     >
                       <b-card-text>
@@ -45,7 +46,7 @@
                 </b-col>
 
                 
-                  <b-col class="col">
+                  <b-col sm="12" md="4">
                   <div class="box">
                     <b-card
                       title="Sign in"
@@ -53,8 +54,8 @@
                       img-alt="Image"
                       img-top
                       tag="article"
-                      style="max-width: 20rem;"
-                      class="mb-2"
+                      style="max-width: 50rem;"
+                      class="mb-2 test123"
                     >
                       <b-card-text>
                         Some quick example text to build on the card title and make up the bulk of the card's content.
@@ -64,7 +65,7 @@
                     </b-card>    
                   </div>
                 </b-col>
-                  <b-col class="col">
+                  <b-col sm="12" md="4">
                   <div class="box">
                     <b-card
                       title="About..."
@@ -72,7 +73,7 @@
                       img-alt="Image"
                       img-top
                       tag="article"
-                      style="max-width: 20rem;"
+                      style="max-width: 50rem;"
                       class="mb-2"
                     >
                       <b-card-text>
@@ -85,57 +86,58 @@
                 </b-col>
                 
               </b-row>
-            </b-container>
+            </div>
         </div>
-        <hr>
-        <div class="box-info">
-            <br><br>
-            <h1>Info</h1>
-            <p>Lorem, ipsum dolor sitamet consectetur adipisicing elit. Odio, velit ab? Alias, recusandae quo. Quos veniam reprehenderit voluptates consectetur consequatur excepturi unde, harum iure. Dignissimos exercitationem esse eaque accusantium fugiat!</p>
-        <br><br>
-        </div>
-        <hr><br><br>
+        <div class="parallax">
+          <div class="box-us">
+          <b-row>
+              <!--First card about us-->
+              <b-col md="6">
+            
+              <div class="card-us">
+                <b-card no-body class="overflow-hidden" style="max-width: 350px;">
+                  <b-row no-gutters>
+                    <b-col md="6">
+                      <b-card-img src="https://image.flaticon.com/icons/svg/2896/2896974.svg" alt="Image" class="rounded-0"  height="200px"></b-card-img>
+                    </b-col>
+                    <b-col md="6">
+                      <b-card-body title="Stanko Bebek">
+                        <b-card-text>
+                          This is a wider card with supporting text as a natural lead-in to additional content.
+                          This content is a little bit longer.
+                        </b-card-text>
+                      </b-card-body>
+                    </b-col>
+                  </b-row>
+                </b-card>
+              </div>
+            
+            </b-col>
+                <!--Second card about us-->
+                <b-col md="6">
+            <div class="card-us">
+                <b-card no-body class="overflow-hidden" style="max-width: 350px;">
+                  <b-row no-gutters>
+                    <b-col md="6">
+                      <b-card-img src="https://image.flaticon.com/icons/svg/2897/2897014.svg" alt="Image" class="rounded-0" height="200px"></b-card-img>
+                    </b-col>
+                    <b-col md="6">
+                      <b-card-body title="Antonio Lončar">
+                        <b-card-text>
+                          This is a wider card with supporting text as a natural lead-in to additional content.
+                          This content is a little bit longer.
+                        </b-card-text>
+                      </b-card-body>
+                    </b-col>
+                  </b-row>
+                </b-card>
+              </div>
 
-        <div class="box-us" >
-          <b-container class="bv-example-row">
-            <b-row>
-              <b-col>
-                                <b-card no-body class="overflow-hidden" style="max-width: 540px;">
-                    <b-row no-gutters>
-                    <b-col md="6">
-                        <b-card-img src="https://picsum.photos/400/400/?image=20" alt="Image" class="rounded-0"></b-card-img>
-                    </b-col>
-                    <b-col md="6">
-                        <b-card-body title="Stanko Bebek">
-                        <b-card-text>
-                            This is a wider card with supporting text as a natural lead-in to additional content.
-                            This content is a little bit longer.
-                        </b-card-text>
-                        </b-card-body>
-                    </b-col>
-                    </b-row>
-                </b-card>
               </b-col>
-              <b-col>
-                                <b-card no-body class="overflow-hidden" style="max-width: 540px;">
-                    <b-row no-gutters>
-                    <b-col md="6">
-                        <b-card-img src="https://picsum.photos/400/400/?image=20" alt="Image" class="rounded-0"></b-card-img>
-                    </b-col>
-                    <b-col md="6">
-                        <b-card-body title="Antonio Lončar">
-                        <b-card-text>
-                            This is a wider card with supporting text as a natural lead-in to additional content.
-                            This content is a little bit longer.
-                        </b-card-text>
-                        </b-card-body>
-                    </b-col>
-                    </b-row>
-                </b-card>
-              </b-col>
-            </b-row>
-          </b-container>
-        </div>         
+          </b-row>
+          </div>
+        </div>   
+        <br />   <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> 
     </main>
 </template>
 
@@ -147,9 +149,9 @@ export default {
 </script>
 
 <style scoped>
+
   .img{
     left: 0;
-    
   }
 
   .landing{
@@ -166,62 +168,101 @@ export default {
   .animation{
     flex: 1;
     justify-content: flex-end;
-   }
+  }
 
-   .side_img{
-     position: absolute;
-     top: 0;
-     left:0;
-     z-index: -1; 
-     height: 60vh;
-     width: 100%;
-   }
+  .side_img{
+    position: absolute;
+    top: 0;
+    left:0;
+    z-index: -1; 
+    height: 60vh;
+    width: 100%;
+  }
 
   .main-section{
-      flex: 3;
+    flex: 3;
   }
-.box-info{
-    max-width: 100%;
-    margin-left: 11%;
-    margin-right: 7%;
-    display: grid;
-    grid-template-columns:repeat(1,1fr);
-    grid-auto-rows: minmax(30px,auto);
-    text-align: center;
-}
-.box-us{
-  margin-bottom: 100px;
-}
-.box-us div{
+  .card-info{
+    color: #ffd800;
+    font-size: 2.3em;
+    margin-top: 2em;
+  }
+
+  .box-us{
+    height: 100%;
+    display: flex;
+    justify-content: center;
     align-items: center;
-}
-.cardbtn{
-border: none;
- background-color: #ffd800;
- cursor: pointer; 
-}
-.boxCard{
-align-items: center;
-margin-top:170px;
-margin-bottom: 60px;
-margin-left: 10%;
-margin-right: 10%;
-}
-/*
-Ovo ne funk bas na onaj nacn koji sam htio al nije lose rijesenje,ako znas
-neko bolje prepravi slobodno :D PS. Radi se o prve 3 kartice ,kada smanjivas ekran
-ne smanjivaju se bas ponajbolje..
-@media screen and (max-width: 850px){
+  }
+  
+  .cardbtn{
+    border: none;
+    background-color: #ffd800;
+    cursor: pointer; 
+  }
+
   .boxCard{
     align-items: center;
-    margin-top:170px;
+    margin-top:50px;
     margin-bottom: 60px;
-    margin-left: 21%;
-    margin-right: 21%;
-    }
+    margin-left: 10%;
+    margin-right: 10%;
+  }
 
+  .parallax{
+    background: url('../assets/dashboard/parallax.jpg') no-repeat center;
+    background-size: cover;
+    background-attachment: fixed;
+    height: 600px;
+  }
+
+  p {
+    word-break: break-word;
 }
 
-*/
+
+  @media only screen and (max-width: 992px) {
+  .animation, .side_img {
+    display: none;
+  }
+
+  .intro-text{
+    margin-top: 100px;
+    font-size: 12px;
+  }
+
+  .intro-text h1{
+    font-size: 32px;
+  }
+
+  .boxCard h3{
+    font-size: 22px;
+  }
+
+  .button-section{
+    text-align: center;
+  }
+
+  .box{
+    display: grid;
+    place-items: center;
+  }
+
+  .parallax{
+    height: 1400px;
+  }
+
+  .box-us div{
+    display: grid;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .box-us div:nth-child(2){
+    margin-top: 2em;
+  }
+  
+
+}
 
 </style>
